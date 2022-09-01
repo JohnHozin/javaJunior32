766860406069bc684f56b45d24b07e7971ecffb6
JohnHozin <simple_protector@mail.ru>
2022-09-02 00:08:56 +0300

    make class work

---
+++ src/lesson21/Main1.java
@@ -0,0 +1,11 @@
+package lesson21;
+
+import org.apache.log4j.Logger;
+public class Main1 {
+    private static final Logger LOGGER = Logger.getLogger(lesson21.Main1.class);
+
+    public static void main(String[] args) {
+        LOGGER.error("Бла бла");
+    }
+
+}
